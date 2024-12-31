953f707bcd258ab03a705ada72ce42207b2c4d31
Arkadiusz Jędrzejewski <32277297+arkjedrz@users.noreply.github.com>
2024-12-31 16:04:23 +0100

    Improve command handling (#11)

--- src/command_runner.rs
+++ src/command_runner.rs
@@ -1,21 +1,30 @@
-use shlex::split;
-use std::io::Error;
-use std::process::{Command, ExitStatus};
+use run_script::types::ScriptError::{Description, FsIOError, IOError};
+use run_script::{spawn, IoOptions, ScriptError, ScriptOptions};
+use std::io::{Error, ErrorKind};
+use std::process::ExitStatus;
+
+/// Convert all errors to `std::io::Error`.
+fn match_error(script_error: ScriptError) -> Error {
+    match script_error {
+        IOError(io_error) => io_error,
+        FsIOError(fsio_error) => Error::new(ErrorKind::Other, fsio_error.to_string()),
+        Description(description) => Error::new(ErrorKind::Other, description),
+    }
+}
 
 /// Spawn new process based on provided resolved command string.
 pub fn run_command(command: &str) -> Result<ExitStatus, Error> {
-    // Split shell command.
-    let split_result = split(command).expect("Failed to split command");
-    // Split command name and arguments.
-    // TODO: support environment variables.
-    let command = &split_result[0];
-    let args = &split_result[1..];
+    // Set parameters.
+    let args = vec![];
+    let mut options = ScriptOptions::new();
+    options.input_redirection = IoOptions::Inherit;
+    options.output_redirection = IoOptions::Inherit;
 
     // Spawn process.
-    let spawn_result = Command::new(command).args(args).spawn();
+    let spawn_result = spawn(command, &args, &options);
     let mut child = match spawn_result {
         Ok(child) => child,
-        Err(e) => return Err(e),
+        Err(e) => return Err(match_error(e)),
     };
 
     // Wait for process to finish.
